be4d687c73a0e5458fdc1f49ad664eec0fa47cfe
Dominik Leszczyński <dominic.les.work@icloud.com>
2026-02-06 18:43:14 +0100

    registering models

--- config/pets/admin.py
+++ config/pets/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Pet, Cat, Dog, Horse
 
 # Register your models here.
+
+admin.site.register(Pet)
+admin.site.register(Cat)
+admin.site.register(Dog)
+admin.site.register(Horse)
